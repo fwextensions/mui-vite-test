94dba55ea7ed8f05cf39e5f3c7ef29af5d45e40f
John Dunning <fw@johndunning.com>
2023-03-27 00:31:56 -0700

    Move index.html into src
    Vite's opinions about this are bad and wrong.
    Use MUI in App.tsx.
    Add node types.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import path from "node:path";
+import process from "node:process";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
+	root: "src",
+	publicDir: "../public",
+	build: {
+		outDir: "../dist"
+	},
+	resolve: {
+		alias: { "/src": path.resolve(process.cwd(), "src") }
+	},
 });
